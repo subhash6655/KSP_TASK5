9bdc5a3627a1ca9904d7ab78facd034a5b12cf6e
Subhash <150155058+subhash6655@users.noreply.github.com>
2023-11-11 15:21:11 +0530

    Add files via upload

---
+++ Task 5/script.js
@@ -0,0 +1,43 @@
+window.alert("My name is Subhash");
+
+console.log("Hello Everyone");
+
+//Variable
+number1 = 4;
+
+console.log(number1);
+
+//let 
+let number = 20;
+
+console.log(number);
+
+//CamelCase
+thisIsTaskNumber = "task number 5";
+
+console.log(thisIsTaskNumber);
+
+//Constant
+const name = "Subhash";
+
+console.log(name);
+
+/*
+
+
+this 
+is 
+multi-line
+comment
+
+*/
+
+// Operators
+let num1 = 6;
+let num2 = 4;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
